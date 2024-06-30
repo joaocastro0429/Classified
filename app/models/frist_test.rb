11e62ac6a930e1bfb5b6a76b72ea5ff55036b411
joaocastro <joaocastro0429@gmail.com>
2024-06-29 21:22:15 -0300

    update project

---
+++ app/models/frist_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class FristTest< ActiveSupport::TestCase
+    test "esse é primeiro teste" do 
+      variavel="joao"
+      
+      assert_equal " o meu nome cadu" , variavel
+    end 
+  
+  end
